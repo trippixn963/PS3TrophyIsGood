ecf3b3b115cc759901a81bdb3199868b04ccea8e
trippixn963 <90400991+trippixn963@users.noreply.github.com>
2026-05-26 11:09:42 -0400

    Make it clear the import is PSNProfiles-only
    Menu item renamed 'Copy from PSNProfiles'; dialog retitled 'Copy from PSNProfiles' with an instruction label ('Paste the donor's PSNProfiles game-trophy URL' + example) above the URL box, Import/Cancel buttons. Also tidied the dialog (compact size, no leftover empty space from the removed smart-copy/JSON controls).

--- PS3TrophyIsGood/CopyFrom.Designer.cs
+++ PS3TrophyIsGood/CopyFrom.Designer.cs
@@ -28,50 +28,68 @@ namespace PS3TrophyIsGood
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CopyFrom));
+            this.urlLabel = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.accept = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             //
+            // urlLabel
+            //
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(12, 14);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Text =
+                "Paste the donor's PSNProfiles game-trophy URL:\r\n"
+                + "e.g.  https://psnprofiles.com/trophies/1-super-stardust-hd/SomeUser";
+            //
             // textBox1
             //
-            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Location = new System.Drawing.Point(13, 52);
             this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 23);
             //
             // accept
             //
-            resources.ApplyResources(this.accept, "accept");
+            this.accept.Location = new System.Drawing.Point(316, 86);
             this.accept.Name = "accept";
+            this.accept.Size = new System.Drawing.Size(75, 27);
+            this.accept.Text = "Import";
             this.accept.UseVisualStyleBackColor = true;
             this.accept.Click += new System.EventHandler(this.accept_Click);
             //
             // button2
             //
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            resources.ApplyResources(this.button2, "button2");
+            this.button2.Location = new System.Drawing.Point(397, 86);
             this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 27);
+            this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
             //
             // CopyFrom
             //
             this.AcceptButton = this.accept;
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button2;
+            this.ClientSize = new System.Drawing.Size(484, 125);
+            this.Controls.Add(this.urlLabel);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.accept);
             this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.Name = "CopyFrom";
+            this.Text = "Copy from PSNProfiles";
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
+        private System.Windows.Forms.Label urlLabel;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button accept;
         private System.Windows.Forms.Button button2;
